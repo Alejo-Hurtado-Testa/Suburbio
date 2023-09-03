06fcf0f3eb63c8a6d7aa141679b485859539bf5e
Alejo <alejohurtadotesta@gmail.com>
2023-09-03 18:15:01 -0300

    Agregamos todos los combos, creamos el js donde los generamos, agreamos mas imagenes, empezamos la pagina de quienes somos, agregamos un mapa y un texto, modificamos algunos estilos de los combos

---
+++ assets/combos.js
@@ -0,0 +1,36 @@
+const listaCombos = document.querySelector('#listcombos');
+
+const listarCombos = async () => {
+  const combos = await pedirCombos();
+  combos.forEach((element) => {
+    const { nombre, img, descripcion } = element;
+    return (listaCombos.innerHTML += `
+        <div class="item-combo">
+                <div class="item-descripcion">
+                  <h2 class="combo-name">${nombre}</h2>
+                  <p class="combo-descripcion">
+                    ${descripcion}
+                  </p>
+                  <button class="btn-combo">Reservar</button>
+                </div>
+                <img
+                  src=${img}
+                  alt="Logo"
+                  class="combo-img"
+                />
+              </div>
+        `);
+  });
+};
+
+async function pedirCombos() {
+  try {
+    const res = await fetch('http://127.0.0.1:5500/assets/combos.json');
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return [];
+  }
+}
+
+listarCombos();
